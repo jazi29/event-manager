8ce157cea2c7306afbac47879752ec8cc76429b0
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-05-05 02:01:07 +0500

    feat: new changes for popup

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ const loginModal = document.getElementById("login-modal");
 const registerModal = document.getElementById("register-modal");
 const eventBlock = document.getElementById("event-block");
 
-const loginButton = document.getElementsByClassName(".login-btn");
-const registerButton = document.getElementsByClassName(".register-btn");
+const loginButton = document.getElementsByClassName("login-btn");
+const registerButton = document.getElementsByClassName("register-btn");
 
-const titleText = document.getElementById("titleText");
-
-// account.addEventListener("click", showLogin());
+const titleText = document.querySelector("h1");
+const main = document.querySelector("main");
 
 function showLogin() {
   loginModal.style.display = "block";
   eventBlock.style.display = "none";
   registerModal.style.display = "none";
   titleText.innerHTML = "Welcome!";
+  main.style.height = "600px";
 }
 
 function showRegister() {
@@ -22,4 +22,26 @@ function showRegister() {
   loginModal.style.display = "none";
   eventBlock.style.display = "none";
   titleText.innerHTML = "Welcome!";
+  main.style.height = "600px";
 }
+
+function hideModal() {
+  eventBlock.style.display = "block";
+  loginModal.style.display = "none";
+  registerModal.style.display = "none";
+}
+
+// document.addEventListener("click", (event) => {
+//   if (!event.target.closest("#login-modal") && event.target.id !== "account") {
+//     hideModal();
+//   }
+// });
+
+// document.addEventListener("click", (event) => {
+//   if (
+//     !event.target.closest("#register-modal") &&
+//     event.target.id !== "account"
+//   ) {
+//     hideModal();
+//   }
+// });
