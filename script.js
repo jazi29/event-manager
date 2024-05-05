6948a67145331721a6b0e7d1716ff861cdd6bd55
Zhazira Satbaeva <79010812+jazi29@users.noreply.github.com>
2024-05-06 03:23:49 +0500

    feat: update eventModal and add register settings

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function showLogin() {
   loginModal.style.display = "block";
   eventBlock.style.display = "none";
   registerModal.style.display = "none";
+  eventMore.style.display = "none";
   titleText.innerHTML = "Welcome!";
   main.style.height = "600px";
 }
@@ -21,6 +22,7 @@ function showRegister() {
   registerModal.style.display = "block";
   loginModal.style.display = "none";
   eventBlock.style.display = "none";
+  eventMore.style.display = "none";
   titleText.innerHTML = "Welcome!";
   main.style.height = "600px";
 }
@@ -37,6 +39,12 @@ function hideModal() {
 //   }
 // });
 
+window.onclick = function (event) {
+  if (event.target == loginModal) {
+    loginModal.style.display = "none";
+  }
+};
+
 // document.addEventListener("click", (event) => {
 //   if (
 //     !event.target.closest("#register-modal") &&
@@ -45,3 +53,59 @@ function hideModal() {
 //     hideModal();
 //   }
 // });
+
+const events = document.querySelectorAll(".event");
+const eventMore = document.getElementById("event-more");
+
+events.forEach(function (event) {
+  event.addEventListener("click", function () {
+    eventShow();
+  });
+});
+
+function eventShow() {
+  eventMore.style.display = "block";
+  eventBlock.style.display = "none";
+  main.style.height = "600px";
+}
+
+const closeButton = document.querySelector(".close-btn");
+
+closeButton.addEventListener("click", function () {
+  eventBlock.style.display = "block";
+  eventMore.style.display = "none";
+  main.style.height = "850px";
+});
+
+// Register and login
+
+const username = document.querySelector("#username");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const submit = document.querySelector("#submit");
+
+let users = {};
+
+function User(username, email, password) {
+  this.username = username;
+  this.email = email;
+  this.password = password;
+}
+
+function createID(users) {
+  return Object.keys(users).length;
+}
+
+submit.addEventListener("click", () => {
+  const loginUser = username.value;
+  const emailUser = email.value;
+  const passwordUser = password.value;
+
+  const user = new User(loginUser, emailUser, passwordUser);
+
+  const userID = "User" + createID(users);
+  users[userID] = user;
+  console.log(users);
+
+  alert(`${loginUser}, you have successfully registered!`);
+});
